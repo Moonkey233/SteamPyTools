09ca37cb2aa41aac92c9842369e357e0f43220f8
Moonkey233 <Moonkey233@foxmail.com>
2025-08-13 22:57:05 +0800

    pay_order支持安全多线程

--- src/pay.py
+++ src/pay.py
@@ -3,74 +3,87 @@ import util
 import const
 import requests
 import list_sale
+import threading
 from config import configs
 
 
 total_price = 0
 total_order = 0
+pay_map     = {}
+pay_lock    = threading.Lock()
 
-pay_map = {}
-
-def pay_order(game_id, price, discount, steam_price):
+def pay_order(game_id, max_price, max_discount, steam_price, confirm_pause=True):
     """根据游戏id发起最优符合条件的支付请求，不符合条件不支付"""
     global total_price, total_order, pay_map
 
-    if game_id in pay_map and pay_map[game_id] > time.time():
-        time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(pay_map[game_id]))
-        return False, f'Already paid for {game_id}, will not repay until {time_str}', 0
-
-    max_budget      = configs.get_pay_config('max_budget', 0)
-    max_order       = configs.get_pay_config('max_order', 0)
-    pay_type        = configs.get_pay_config('pay_type', 'AL')
-    promo_code_id   = configs.get_pay_config('promo_code_id', '')
-    use_balance     = configs.get_pay_config('use_balance', False)
-    confirm_pause   = configs.get_pay_config('confirm_pause', True)
-
-    if total_price >= max_budget or total_order >= max_order:
-        return False, f'Out of Budget: {total_price}r/{max_budget}r {total_order}/{max_order}', -1
-
+    max_budget = configs.get_pay_config('max_budget', 0)
+    max_order = configs.get_pay_config('max_order', 0)
+    pay_type = configs.get_pay_config('pay_type', 'AL')
+    promo_code_id = configs.get_pay_config('promo_code_id', '')
+    use_balance = configs.get_pay_config('use_balance', False)
     data = {
-        'payType':      pay_type,
-        'promoCodeId':  promo_code_id,
+        'payType': pay_type,
+        'promoCodeId': promo_code_id,
     }
-
     if use_balance:
         data['walletFlag'] = 'useBalance'
 
-    for order in list_sale.get_list_sale(game_id):
-        key_price = float(util.get_json_value(order, 'keyPrice', ''))
-        real_discount = key_price / float(steam_price)
+    # 加读锁
+    with pay_lock:
+        # 一个game_id只有一个线程，pay_map读安全，优先用时间筛掉重复支付
+        if game_id in pay_map and pay_map[game_id] > time.time():
+            time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(pay_map[game_id]))
+            return False, f'Already paid for {game_id}, will not repay until {time_str}', -2
 
-        if (key_price > price or
-                real_discount > discount or
-                total_price + key_price > max_budget):
-            break
+        # 粗估计，筛掉一部分
+        if total_price >= max_budget or total_order >= max_order:
+            return False, f'Out of Budget: {total_price}r/{max_budget}r {total_order}/{max_order}', -1
 
-        data['saleId'] = util.get_json_value(order, 'saleId', '')
+    # 最耗时的一步，不加锁
+    order_list = list_sale.get_list_sale(game_id)
 
-        print(f'[Real Price]: {key_price}, [Real Discount]: {real_discount:.2f}')
-        if confirm_pause:
-            if configs.get_pay_config('pause_beep', False):
-                util.beep()
-            input('Press Enter to Continue: ')
+    # 加锁操作，较少订单会进入post，post调用较少，因此post一起锁住保证一致性
+    with pay_lock:
+        for order in order_list:
+            key_price = float(util.get_json_value(order, 'keyPrice', ''))
+            real_discount = key_price / float(steam_price)
 
-        payResp = requests.post(
-            url     = const.py_pay_order_url,
-            headers = const.py_headers,
-            cookies = const.py_cookies,
-            data    = data
-        )
-        pay_data = payResp.json()
+            # 绝大多数在此返回
+            if key_price > max_price or real_discount > max_discount:
+                break
 
-        print(pay_data)
+            # 再次精确判定预算
+            if total_price + key_price > max_budget:
+                break
 
-        if (payResp.status_code == 200 and
-                util.get_json_value(pay_data, ['message'], '') == 'success' and
-                util.get_json_value(pay_data, ['success'], False)):
-            pay_price = float(util.get_json_value(pay_data, ['result', 'payPrice'], ''))
-            total_price += pay_price
-            total_order += 1
-            pay_map[game_id] = time.time() + configs.get_pay_config('pay_time', 2000)
-            return True, util.get_json_value(pay_data, ['result', 'orderId'], ''), pay_price
+            data['saleId'] = util.get_json_value(order, 'saleId', '')
+
+            print(f'[Real Price]: {key_price}, [Real Discount]: {real_discount:.2f}')
+            if confirm_pause:
+                if configs.get_pay_config('pause_beep', False):
+                    util.beep()
+                input('Press Enter to Continue: ')
+
+            pay_resp = requests.post(
+                url     = const.py_pay_order_url,
+                headers = const.py_headers,
+                cookies = const.py_cookies,
+                data    = data
+            )
+            pay_data = pay_resp.json()
+
+            print(pay_data)
+
+            if (pay_resp.status_code == 200 and
+                    util.get_json_value(pay_data, ['message'], '') == 'success' and
+                    util.get_json_value(pay_data, ['success'], False)):
+                pay_price = float(util.get_json_value(pay_data, ['result', 'payPrice'], ''))
+
+                # 写入预算
+                total_price += key_price
+                total_order += 1
+                pay_map[game_id] = time.time() + configs.get_pay_config('pay_time', 2000)
+
+                return True, util.get_json_value(pay_data, ['result', 'orderId'], ''), pay_price
 
     return False, 'No Orders Meet the Filter', 0
